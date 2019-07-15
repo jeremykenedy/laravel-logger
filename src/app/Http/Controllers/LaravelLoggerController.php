41e9b2b76cd2fb69b0b4a38ef14782de27a2b4dc
Mohamed Abouda <mohamed.aboda500@gmail.com>
2019-07-15 13:59:41 +0200

    Update LaravelLoggerController.php

--- src/app/Http/Controllers/LaravelLoggerController.php
+++ src/app/Http/Controllers/LaravelLoggerController.php
@@ -67,7 +67,7 @@ class LaravelLoggerController extends BaseController
     {
         if (config('LaravelLogger.loggerPaginationEnabled')) {
             $activities = Activity::orderBy('created_at', 'desc');
-            if (config('LaravelLogger.disableSearch')) {
+            if (config('LaravelLogger.enableSearch')) {
                 $activities = $this->searchActivityLog($activities, $request);
             }
             $activities = $activities->paginate(config('LaravelLogger.loggerPaginationPerPage'));
@@ -75,7 +75,7 @@ class LaravelLoggerController extends BaseController
         } else {
             $activities = Activity::orderBy('created_at', 'desc');
 
-            if (config('LaravelLogger.disableSearch')) {
+            if (config('LaravelLogger.enableSearch')) {
                 $activities = $this->searchActivityLog($activities, $request);
             }
             $activities = $activities->get();
@@ -280,29 +280,29 @@ class LaravelLoggerController extends BaseController
      *
      * @return filtered query
      */
-    public function searchActivityLog($query, $requeset)
-    {
-        if (config('LaravelLogger.descriptionSearch') && $requeset->get('description')) {
-            $query->where('description', 'like', '%'.$requeset->get('description').'%');
+      public function searchActivityLog($query,$requeset)
+      {
+            if (in_array('description',explode(',', config('LaravelLogger.searchFields'))) && $requeset->get('description')) {
+                $query->where('description','like','%'.$requeset->get('description').'%');
+            }
+
+
+            if (in_array('user',explode(',', config('LaravelLogger.searchFields'))) && $requeset->get('user')) {
+                $query->where('userId','=',$requeset->get('user'));
+            }
+
+            if (in_array('method',explode(',', config('LaravelLogger.searchFields'))) && $requeset->get('method')) {
+                $query->where('methodType','=',$requeset->get('method'));
+            }
+
+            if (in_array('route',explode(',', config('LaravelLogger.searchFields')))&& $requeset->get('route')) {
+                $query->where('route','like','%'.$requeset->get('route').'%');
+            }
+
+            if (in_array('ip',explode(',', config('LaravelLogger.searchFields'))) && $requeset->get('ip_address')) {
+                $query->where('ipAddress','like','%'.$requeset->get('ip_address').'%');
+            }
+
+            return $query;
         }
-
-
-        if (config('LaravelLogger.userSearch') && $requeset->get('user')) {
-            $query->where('userId', '=', $requeset->get('user'));
-        }
-
-        if (config('LaravelLogger.methodSearch') && $requeset->get('method')) {
-            $query->where('methodType', '=', $requeset->get('method'));
-        }
-
-        if (config('LaravelLogger.routeSearch') && $requeset->get('route')) {
-            $query->where('route', 'like', '%'.$requeset->get('route').'%');
-        }
-
-        if (config('LaravelLogger.ipAddressSearch') && $requeset->get('ip_address')) {
-            $query->where('ipAddress', 'like', '%'.$requeset->get('ip_address').'%');
-        }
-
-        return $query;
     }
-}
